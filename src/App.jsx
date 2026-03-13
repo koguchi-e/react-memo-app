1d1cc39cee8a493cd02a05e4835fa919afedf098
koguchi-e <koguchi.e915@gmail.com>
2026-03-13 11:44:39 +0900

    更新ボタンを追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ function App() {
 
   return (
     <div className="app-background">
+      <label htmlFor="memo-input" className="label">
+        <b>新規作成</b>
+      </label>
       <div className="container">
-        <label htmlFor="memo-input" className="label">
-          新規作成
-        </label>
         <textarea
           id="memo-input"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           className="textarea-form"
+          rows={5}
         ></textarea>
+        <button type="submit" className="button">
+          更新
+        </button>
       </div>
     </div>
   );
